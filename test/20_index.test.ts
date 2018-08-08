4847245b15561c218e994156c8e555832cef5b88
waiting <1661926154@qq.com>
2018-08-08 10:05:44 +0800

    test: split ab2str() to util.ts

--- test/20_index.test.ts
+++ test/20_index.test.ts
@@ -11,6 +11,7 @@ import {
 } from '../src/shared/index'
 
 import { HttpbinGetResponse, HttpbinPostResponse } from './model'
+import { ab2str } from './util'
 
 
 const filename = basename(__filename)
@@ -233,17 +234,4 @@ describe(filename, () => {
 
   })
 
-
 })
-
-
-
-function ab2str(buf: ArrayBuffer) {
-  const bufView = new Uint8Array(buf)
-  const len = bufView.length
-  const bstr = new Array(len)
-  for (let i = 0; i < len; i++) {
-    bstr[i] = String.fromCharCode.call(null, bufView[i])
-  }
-  return bstr.join('')
-}
--- test_browser/20_index.test.ts
+++ test_browser/20_index.test.ts
@@ -175,16 +175,4 @@ describe(filename, () => {
 
   })
 
-
 })
-
-
-function ab2str(buf: ArrayBuffer) {
-  const bufView = new Uint8Array(buf)
-  const len = bufView.length
-  const bstr = new Array(len)
-  for (let i = 0; i < len; i++) {
-    bstr[i] = String.fromCharCode.call(null, bufView[i])
-  }
-  return bstr.join('')
-}
