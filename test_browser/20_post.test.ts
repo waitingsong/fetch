bd96e7b5e3ee6fcb5987aee56f025156be2c3faf
waiting <1661926154@qq.com>
2018-08-08 15:44:43 +0800

    test: split

--- test_browser/20_post.test.ts
+++ test_browser/20_post.test.ts
@@ -1,79 +1,13 @@
 /// <reference types="mocha" />
 
 import * as assert from 'power-assert'
-import * as QueryString from 'qs'
 
-import { get, post, RxRequestInit } from '../src/index'
-
-import { HttpbinGetResponse, HttpbinPostResponse } from '../test/model'
+import { post, RxRequestInit } from '../src/index'
+import { HttpbinPostResponse } from '../test/model'
 
 const filename = '20_index.test.ts'
 
 describe(filename, () => {
-
-  describe('Should get() works with httpbin.org', () => {
-    const url = 'https://httpbin.org/get'
-    const pdata = {
-      p1: Math.random(),
-      p2: Math.random().toString(),
-    }
-    const initArgs = <RxRequestInit> {
-      timeout: 20 * 1000,
-      dataType: 'json',
-    }
-
-    it('without args', resolve => {
-      get<HttpbinGetResponse>(url).subscribe(
-        res => {
-          assert(!! res, 'Should response not empty')
-          assert(res.url === url)
-        },
-        err => {
-          assert(false, err)
-          resolve()
-        },
-        resolve,
-      )
-    })
-
-    it('without query data', resolve => {
-      const args = { ...initArgs }
-
-      get<HttpbinGetResponse>(url, args).subscribe(
-        res => {
-          assert(!! res, 'Should response not empty')
-          assert(res.url === url)
-        },
-        err => {
-          assert(false, err)
-          resolve()
-        },
-        resolve,
-      )
-    })
-
-    it('with query data', resolve => {
-      const args = { ...initArgs }
-      args.data = pdata
-
-      get<HttpbinGetResponse>(url, args).subscribe(
-        res => {
-          // console.info(res)
-          assert(res && res.args, 'Should response.args not empty')
-          assert(res.url === url + '?' + QueryString.stringify(pdata))
-          assert(res.args.p1 === pdata.p1.toString(), `Should got ${pdata.p1}`)
-          assert(res.args.p2 === pdata.p2, `Should got ${pdata.p2}`)
-        },
-        err => {
-          assert(false, err)
-          resolve()
-        },
-        resolve,
-      )
-    })
-
-  })
-
 
   describe('Should post() works with httpbin.org', () => {
     const url = 'https://httpbin.org/post'
