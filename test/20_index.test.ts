d6fe1f9d6a34981d6c77f9553e23af9f14439e36
waiting <1661926154@qq.com>
2018-08-08 19:17:08 +0800

    test: 20_index.test.ts
    getGloalRequestInit(), setGloalRequestInit()

---
+++ test/20_index.test.ts
@@ -0,0 +1,68 @@
+/// <reference types="mocha" />
+
+import * as assert from 'power-assert'
+
+import { getGloalRequestInit, setGloalRequestInit } from '../src/index'
+import { initialRxRequestInit } from '../src/lib/config'
+import { basename } from '../src/shared/index'
+
+
+const filename = basename(__filename)
+const defaultInit = getGloalRequestInit()
+
+describe(filename, () => {
+  after(() => {
+    setGloalRequestInit(defaultInit)
+  })
+
+  describe('Should getGloalRequestInit() works', () => {
+    const initData = getGloalRequestInit()
+
+    it('result is copy of initialRxRequestInit', () => {
+      assert(initData && initData !== initialRxRequestInit)
+    })
+
+    it('values of result equal to initialRxRequestInit', () => {
+      for (const [key, value] of Object.entries(initData)) {
+        if (! (key in initialRxRequestInit)) {
+          assert(false, `${key} not exists in initialRxRequestInit`)
+          break
+        }
+        else {
+          const d = Object.getOwnPropertyDescriptor(initialRxRequestInit, key)
+          assert(d && value === d.value, `key: "${key}": not equal`)
+        }
+      }
+    })
+  })
+
+
+  describe('Should setGloalRequestInit() works', () => {
+    it('change method to POST', () => {
+      const method = 'POST'
+      setGloalRequestInit({ method })
+      const initData = getGloalRequestInit()
+
+      assert(initData && initData.method === method)
+    })
+
+    it('change mode to no-cors', () => {
+      const { mode: oriMode } = getGloalRequestInit()
+      const mode = 'no-cors'
+      assert(oriMode !== mode)
+      setGloalRequestInit({ mode })
+      const { mode: curMode } = getGloalRequestInit()
+
+      assert(curMode === mode)
+    })
+
+    it('change timeout', () => {
+      const timeout = Math.ceil(Math.random() * 100)
+      setGloalRequestInit({ timeout })
+      const initData = getGloalRequestInit()
+
+      assert(initData && initData.timeout === timeout)
+    })
+  })
+
+})
