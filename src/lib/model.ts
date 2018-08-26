b8c6b997a3af55d23585b694b028d1a52c22aca1
waiting <1661926154@qq.com>
2018-08-26 18:24:22 +0800

    chore: update comments

--- src/lib/model.ts
+++ src/lib/model.ts
@@ -3,37 +3,37 @@ export type ObbRetType = ArrayBuffer | Blob | FormData | Response | string | obj
 export interface RespDataType {
   arrayBuffer: ArrayBuffer
   blob: Blob
-  /** Not supported with fetch polyfill */
+  /** Not supported with fetch polyfill yet */
   formData: FormData
   json: object
   text: string
   raw: Response
 }
 
-/** see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type */
+/** See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type */
 export type ContentType = string
 
 export interface Args {
-  /** instance of AbortController to aborting Request */
+  /** Instance of AbortController to aborting Request */
   abortController?: AbortController
 
-  /** append custom cookies with key:value object */
+  /** Append custom cookies with key:value object */
   cookies?: { [key: string]: string | number | null }
 
   /** Content-Type, jQuery behavior, default "application/x-www-form-urlencoded; charset=UTF-8" during POST */
   contentType?: false | ContentType
 
-  /** send to server, resolve to query string during GET|DELETE and key/value pairs during POST */
+  /** Send to server, resolve to query string during GET|DELETE and key/value pairs during POST */
   data?: object
 
   /**
-   * expect data type returned from server. jQuery behavior.
-   * default "json"
-   * return Response object without parse if set to "raw"
+   * Expect data type returned from server. jQuery behavior.
+   * Default "json"
+   * Return Response object without parse if set to "raw"
    */
   dataType?: 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text' | 'raw'
 
-  /** pass a fetch() module for isomorphic usage such as node-fetch or isomorphic-fetch */
+  /** Pass a fetch() module for isomorphic usage such as node-fetch or isomorphic-fetch */
   fetchModule?: (input: string | Request, init?: RequestInit) => Promise<Response | any>
   headersInitClass?: typeof Headers | HeadersFixed | any
 
@@ -48,7 +48,7 @@ export interface Args {
   /** whether process Args.data automatically. jQuery behavior */
   processData?: boolean
 
-  /** msec default Infinity */
+  /** msec Default value Infinity */
   timeout?: number | null
 }
 
@@ -57,7 +57,7 @@ export interface RxRequestInit extends RequestInit, Args {
   referrer?: 'client' | 'no-referrer'
 }
 
-/** for karma test */
+/** For karma test */
 export interface HeadersFixed extends Headers {
   [prop: string]: any
 }
