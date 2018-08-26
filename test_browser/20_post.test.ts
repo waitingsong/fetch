7c7aed5cc4a08820907525ad4f6cc2383a8b5cae
waiting <1661926154@qq.com>
2018-08-26 14:41:44 +0800

    test: increase wait time

--- test_browser/20_post.test.ts
+++ test_browser/20_post.test.ts
@@ -9,7 +9,7 @@ const filename = '20_post.test.ts'
 
 describe(filename, function() {
   this.retries(3)
-  beforeEach(resolve => setTimeout(resolve, 2000))
+  beforeEach(resolve => setTimeout(resolve, 5000))
 
   describe('Should post() works with httpbin.org', () => {
     const url = 'https://httpbin.org/post'
