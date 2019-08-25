71b8f8c06de56c979858aa5c819e1ff8696dbe1c
waiting <1661926154@qq.com>
2019-08-25 14:29:45 +0800

    chore(lint): remove 'sentence-case' from subject-case

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,7 +8,7 @@ const config = {
     'subject-case': [
       2,
       'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+      ['start-case', 'pascal-case', 'upper-case'],
     ],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
