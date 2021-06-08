c0f62d65807ba668066cb164b11ae1f425758faa
waiting <1661926154@qq.com>
2021-06-08 18:04:17 +0800

    feat(midway-component-fetch): rename span name to FetchComponent
    from fetchComponent

--- packages/midway-component-fetch/src/lib/helper.ts
+++ packages/midway-component-fetch/src/lib/helper.ts
@@ -31,7 +31,7 @@ const beforeRequest: FetchComponentConfig['beforeRequest'] = async (options) => 
   ctx.tracerManager.spanLog(input)
 
   const { tracerManager, fetchRequestSpanMap } = ctx
-  const span = tracerManager.genSpan('fetchComponent')
+  const span = tracerManager.genSpan('FetchComponent')
   const tags: SpanLogInput = {
     [Tags.HTTP_URL]: opts.url,
     [Tags.HTTP_METHOD]: opts.method,
