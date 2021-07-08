273742ca5b71c886610a753f904d628836fd01a1
waiting <1661926154@qq.com>
2021-07-08 20:30:25 +0800

    feat(midway-component-fetch): use namespace 'fetch'

--- packages/midway-component-fetch/src/configuration.ts
+++ packages/midway-component-fetch/src/configuration.ts
@@ -5,7 +5,10 @@ import { join } from 'path'
 import { Configuration } from '@midwayjs/decorator'
 
 
+const namespace = 'fetch'
+
 @Configuration({
+  namespace,
   importConfigs: [join(__dirname, 'config')],
 })
 export class AutoConfiguration {
