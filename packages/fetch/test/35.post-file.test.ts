1e95a8131f4e369ff5b11b5099ca5bbb69a427b2
waiting <1661926154@qq.com>
2021-12-07 09:41:13 +0800

    test(fetch): post Form contains image via Stream

---
+++ packages/fetch/test/35.post-file.test.ts
@@ -0,0 +1,114 @@
+import { createReadStream } from 'fs'
+import { relative } from 'path'
+
+import { join } from '@waiting/shared-core'
+import FormData from 'form-data'
+
+import { post, Options, ContentTypeList } from '../src/index'
+
+import { DELAY, HOST_POST } from './config'
+import { HttpbinPostResponse, PostForm1 } from './test.types'
+
+// eslint-disable-next-line import/order
+import assert = require('power-assert')
+
+
+const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
+
+describe(filename, function() {
+  this.retries(3)
+  beforeEach(resolve => setTimeout(resolve, DELAY))
+
+  describe('Should post() work with httpbin.org', () => {
+    const url = HOST_POST
+    const initOpts = {
+      contentType: ContentTypeList.formDataPartial,
+    } as Options
+
+    it('post Form contains an image file via Stream', (done) => {
+      const path = join(__dirname, 'images/loading-1.gif')
+      const stream1 = createReadStream(path)
+      const stream2 = createReadStream(path)
+
+      const pdata = new FormData()
+      const p1 = Math.random()
+      const p2 = Math.random().toString()
+      pdata.append('p1', p1)
+      pdata.append('p2', p2)
+      pdata.append('uploadFile', stream1)
+      const opts: Options = {
+        ...initOpts,
+        data: pdata,
+        processData: false,
+        contentType: false,
+      }
+
+      let buf: Buffer = Buffer.alloc(0)
+
+      stream2.on('data', (data: Buffer) => {
+        buf = Buffer.concat([buf, data])
+      })
+      stream2.on('error', () => {
+        assert(false, 'read file failed')
+        done()
+      })
+
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      stream2.on('end', async () => {
+
+        const res = await post<HttpbinPostResponse<PostForm1>>(url, opts)
+        assert(res && res.url === url)
+        const { form } = res
+        assert(form && form.p1 === p1.toString(), `Should got "${p1}"`)
+        assert(form && form.p2 === p2, `Should got "${p2}"`)
+
+        if (! res.files) {
+          assert(false)
+          return
+        }
+        const { uploadFile } = res.files
+        assert(typeof uploadFile === 'string' && uploadFile)
+        // console.log({ res })
+        const base64 = buf.toString('base64')
+        assert(
+          uploadFile === 'data:image/gif;base64,' + base64,
+          `Should get "${base64.slice(0, 50)}..." but got ${res && res.data}`,
+        )
+
+        done()
+      })
+    })
+
+    it('post Form contains an image file via Buffer', (done) => {
+      const path = join(__dirname, 'images/loading-1.gif')
+      const readerStream = createReadStream(path)
+      const readerStream2 = createReadStream(path)
+      const args: Options = {
+        ...initOpts, data: readerStream, processData: false, contentType: false,
+      }
+      let buf: Buffer = Buffer.alloc(0)
+
+      readerStream2.on('data', (data: Buffer) => {
+        buf = Buffer.concat([buf, data])
+      })
+      readerStream2.on('error', () => {
+        assert(false, 'read file failed')
+        done()
+      })
+
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      readerStream2.on('end', async () => {
+        const res = await post<HttpbinPostResponse>(url, args)
+        const base64 = buf.toString('base64')
+        assert(res && res.url === url)
+        assert(
+          res.data && res.data === 'data:application/octet-stream;base64,' + base64,
+          `Should get "${base64.slice(0, 50)}..." but got ${res && res.data}`,
+        )
+
+        done()
+      })
+    })
+  })
+
+})
