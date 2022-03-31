03f44317787ff6a2b8b4aca7ff51d6130eca56fa
waiting <1661926154@qq.com>
2022-04-01 00:08:30 +0800

    fix(midway-component-fetch): import

--- packages/midway-component-fetch/src/configuration.ts
+++ packages/midway-component-fetch/src/configuration.ts
@@ -4,7 +4,7 @@ import { join } from 'path'
 
 import { Configuration } from '@midwayjs/decorator'
 
-import { ConfigKey } from '~/index'
+import { ConfigKey } from './lib/index'
 
 
 @Configuration({
