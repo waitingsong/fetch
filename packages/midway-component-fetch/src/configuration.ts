2741e8b542aaab5b132c64db9883576fefc72857
waiting <1661926154@qq.com>
2022-08-25 00:28:05 +0800

    chore(midway-component-fetch): update src/configuration.ts
    not inject TracerComponent

--- packages/midway-component-fetch/src/configuration.ts
+++ packages/midway-component-fetch/src/configuration.ts
@@ -2,8 +2,7 @@ import 'tsconfig-paths/register'
 
 import { join } from 'path'
 
-import { Configuration, Inject } from '@midwayjs/decorator'
-import * as jaeger from '@mw-components/jaeger'
+import { Configuration } from '@midwayjs/decorator'
 
 import { ConfigKey } from './lib/index'
 
@@ -11,11 +10,7 @@ import { ConfigKey } from './lib/index'
 @Configuration({
   namespace: ConfigKey.namespace,
   importConfigs: [join(__dirname, 'config')],
-  imports: [jaeger],
 })
 export class AutoConfiguration {
-
-  @Inject() readonly jaeger: jaeger.TracerComponent
-
 }
 
--- packages/midway-component-fetch/test/fixtures/base-app/src/configuration.ts
+++ packages/midway-component-fetch/test/fixtures/base-app/src/configuration.ts
@@ -1,12 +1,17 @@
 import { join } from 'node:path'
 
 import 'tsconfig-paths/register'
-import { Configuration } from '@midwayjs/decorator'
+import { Configuration, Inject } from '@midwayjs/decorator'
+import * as jaeger from '@mw-components/jaeger'
 
 
 @Configuration({
-  imports: [ require('../../../../src'), ],
+  imports: [
+    require('../../../../src'),
+    jaeger,
+  ],
   importConfigs: [join(__dirname, 'config')],
 })
 export class AutoConfiguration {
+  @Inject() readonly jaeger: jaeger.TracerComponent
 }
