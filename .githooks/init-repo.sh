09bd8b284c7fa05da327c22a6635a6668a496141
waiting <1661926154@qq.com>
2023-07-25 11:31:45 +0800

    ci: update init-repo.sh

--- .githooks/init-repo.sh
+++ .githooks/init-repo.sh
@@ -13,6 +13,7 @@ git config --global i18n.commitencoding utf-8
 if [ -z "$CI" ]; then
   git config --local core.autocrlf false
   git config --local core.eol lf
+  git config --local core.filemode false
   git config --local core.hooksPath ./.githooks
   git config --local core.ignorecase false
   git config --local core.precomposeUnicode true
