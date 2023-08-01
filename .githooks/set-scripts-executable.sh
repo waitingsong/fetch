9f37ce76711a8308ba6c23351bae8e8cd7f3aaba
waiting <1661926154@qq.com>
2023-08-01 14:15:03 +0800

    chore: update script and packages/mw-demo/package.json

--- .githooks/set-scripts-executable.sh
+++ .githooks/set-scripts-executable.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 set +e
 
-find .scripts -type f -iname "*.sh" -print0 \
-  | xargs -0II git update-index --ignore-missing --chmod=+x I
+dirs=" .githooks .scripts .github"
 
-find .scripts -type f -iname "*.mjs" -print0 \
-  | xargs -0II git update-index --ignore-missing --chmod=+x I
-
-find .github -type f -iname "*.sh" -print0 \
-  | xargs -0II git update-index --ignore-missing --chmod=+x I
-
-find .github -type f -iname "*.mjs" -print0 \
-  | xargs -0II git update-index --ignore-missing --chmod=+x I
-
-find .githooks -type f -iname "*.sh" -print0 \
-  | xargs -0II git update-index --ignore-missing --chmod=+x I
+for dir in $dirs; do
+  find $dir -type f -iname "*.sh" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+  find $dir -type f -iname "*.mjs" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+done
 
 echo "Commit changes if changed!"
 
