be49cbaf38b726be40cccb1b93864f77c8d8e7d5
waiting <1661926154@qq.com>
2023-09-04 10:10:03 +0800

    test(boilerplate): update mw-demo/test/02.home.test.ts

--- packages/mw-demo/test/02.home.test.ts
+++ packages/mw-demo/test/02.home.test.ts
@@ -3,7 +3,7 @@ import assert from 'node:assert/strict'
 import { fileShortPath } from '@waiting/shared-core'
 
 import { ConfigKey, Msg } from '##/lib/types.js'
-import { TestRespBody, testConfig } from '#@/root.config.js'
+import { RespData, TestRespBody, testConfig } from '#@/root.config.js'
 
 
 describe(fileShortPath(import.meta.url), function() {
@@ -18,15 +18,24 @@ describe(fileShortPath(import.meta.url), function() {
       .get(path)
       .expect(200)
 
-    const ret = resp.body as TestRespBody
+    const ret = resp.body as TestRespBody | RespData
     assert(typeof ret === 'object', JSON.stringify(ret, null, 2))
-    assert(ret.code === 0)
-    assert(typeof ret.data === 'object', JSON.stringify(ret, null, 2))
 
-    const { url, header } = ret.data
-    const { host } = header
-    assert(url === '/', JSON.stringify(ret, null, 2))
-    assert(host && testConfig.host.includes(host), JSON.stringify(ret, null, 2))
+    if ('code' in ret && typeof ret.code === 'number') {
+      assert(ret.code === 0)
+      assert(typeof ret.data === 'object', JSON.stringify(ret, null, 2))
+
+      const { url, header } = ret.data
+      const { host } = header
+      assert(url === '/', JSON.stringify(ret, null, 2))
+      assert(host && testConfig.host.includes(host), JSON.stringify(ret, null, 2))
+    }
+    else {
+      const { url, header } = ret as RespData
+      const { host } = header
+      assert(url === '/', JSON.stringify(ret, null, 2))
+      assert(host && testConfig.host.includes(host), JSON.stringify(ret, null, 2))
+    }
   })
 
   it(`Should ${helloPath} work`, async () => {
