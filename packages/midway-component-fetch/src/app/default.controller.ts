02391a671c9723ac38017127319b34105e2421d5
waiting <1661926154@qq.com>
2024-02-26 09:59:07 +0800

    chore(mwcp): change name of DefaultComponentController to DefaultFetchController

--- packages/midway-component-fetch/src/app/default.controller.ts
+++ packages/midway-component-fetch/src/app/default.controller.ts
@@ -8,17 +8,17 @@ import { Config, ConfigKey, Msg } from '##/lib/types.js'
 
 
 @Controller(`/_${ConfigKey.namespace}`)
-export class DefaultComponentController {
+export class DefaultFetchController {
 
   @_Config(ConfigKey.config) readonly config: Config
 
   @Get('/hello')
   hello(): string {
-    // this.valiateRoute()
+    // this.validateRoute()
     return Msg.hello
   }
 
-  // valiateRoute(): void {
+  // validateRoute(): void {
   //   if (! this.config.enableDefaultRoute) {
   //     throw new Error('route is not enabled')
   //   }
